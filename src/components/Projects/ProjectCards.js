4271f9217e79d6c704c27a6a65c15d58092ae466
Liang <lianginchina1999@gmail.com>
2024-04-23 15:17:51 +0800

    add techUsed prop

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -1,15 +1,21 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import Badge from 'react-bootstrap/Badge';
+import Stack from 'react-bootstrap/Stack';
 import { CgWebsite } from "react-icons/cg";
 import { BsGithub } from "react-icons/bs";
 
 function ProjectCards(props) {
+  console.log(props)
   return (
     <Card className="project-card-view">
       <Card.Img variant="top" src={props.imgPath} alt="card-img" />
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
+        <Stack direction="horizontal" gap={2} style={{margin:'1em', flexWrap:'wrap'}}>
+          {props.techUsed.map(tech=><Badge bg="secondary">{tech}</Badge>)}
+        </Stack>
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
         </Card.Text>
